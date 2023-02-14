2e3740c38c42d08fb0433341be16338a3399fabd
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-02-14 19:25:58 +0400

    crud done, error validation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,7 @@
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto {
+  @IsNotEmpty({ message: 'Task title is required' })
   title: string;
+  @IsNotEmpty({ message: 'Task description is required' })
   description: string;
 }
