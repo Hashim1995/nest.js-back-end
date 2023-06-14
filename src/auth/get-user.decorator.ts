7deebc973c3471768cd38acca71acc3ed5b1f675
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-06-14 15:59:20 +0400

    changes

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
+// get user decorator
 export const GetUserDecorator = createParamDecorator(
   (_data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
